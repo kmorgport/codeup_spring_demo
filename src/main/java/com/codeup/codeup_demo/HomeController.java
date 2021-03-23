c4171bd819210be9888d051f94155b8a02edb6c5
Kole Morgan <kmorgan0057@gmail.com>
2021-03-23 14:38:38 -0500

    Initial commit

---
+++ src/main/java/com/codeup/codeup_demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.codeup.codeup_demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String landing() {
+        return "This is the landing page!";
+    }
+
+}
