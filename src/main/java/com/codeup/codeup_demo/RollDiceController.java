5072d68dfc108dcd29afa57b6b556c8fe955698f
Kole Morgan <kmorgan0057@gmail.com>
2021-03-24 12:19:19 -0500

    spring boot practice

--- src/main/java/com/codeup/codeup_demo/RollDiceController.java
+++ src/main/java/com/codeup/codeup_demo/RollDiceController.java
@@ -15,6 +15,10 @@ class RollDiceController {
     @PostMapping("/roll-dice")
     public String rolldice(@RequestParam(name="dice") String dice, Model model) {
         model.addAttribute("dice","Your dice number is "+dice+"!");
+        int random = (int) ((Math.random() * (5)) + 1);
+        System.out.println(random);
+        model.addAttribute("diceroll","The dice roll is "+ random+"!");
+        model.addAttribute("correct",dice.equals(String.valueOf(random)));
         return "roll-dice";
     }
 
